8991642292c937290159f5f5333d919206debc2f
Iqbolshoh <iilhomjonov777@gmail.com>
2024-12-17 22:51:38 +0500

    commit

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
-const lines = document.querySelector(".lines");
-const navMenu = document.querySelector(".nav-menu");
+const lines = document.querySelector('.lines');
+const navMenu = document.querySelector('.nav-menu');
 
-lines.addEventListener("click", mobileMenu);
-
-function mobileMenu() {
-    lines.classList.toggle("active");
-    navMenu.classList.toggle("active");
-}
-const navLink = document.querySelectorAll(".nav-link");
-
-navLink.forEach(n => n.addEventListener("click", closeMenu));
-
-function closeMenu() {
-    lines.classList.remove("active");
-    navMenu.classList.remove("active");
-}
+lines.addEventListener('click', () => {
+    lines.classList.toggle('active');
+    navMenu.classList.toggle('active');
+});
